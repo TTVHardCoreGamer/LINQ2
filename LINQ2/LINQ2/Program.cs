1e5f5c1275d5e1348ba40f391d258a00d39c3750
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-03-19 15:47:36 +0200

    Where

--- LINQ2/LINQ2/Program.cs
+++ LINQ2/LINQ2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1. Where");
+
+            int choise = int.Parse(Console.ReadLine());
+
+            switch (choise)
+            {
+                case 1:
+                    Where();
+                    break;
+
+                default:
+                    Console.WriteLine("Vale valik");
+                    break;
+            }
+        }
+        //where paneb konsooli asjad mis vastavad mingile tingimusele ehk siis
+        //, et näiteks kus on hind 60 siis leiab need ja paneb konsooli
+        public static void Where()
+        {
+            var where = ProductList.Products.Where(x => x.Price > 60);
+
+            foreach (var item in where)
+            {
+                Console.WriteLine(item.Price);
+            }
         }
     }
 }
+
+
+
+
